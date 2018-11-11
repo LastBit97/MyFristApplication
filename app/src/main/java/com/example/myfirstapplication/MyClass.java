d7e17582b1d96411f98a0944d162946559da0e31
Dmitry Odinokov <ostbit@yandex.ru>
2018-11-11 23:03:56 +0300

    commit newbranch

--- app/src/main/java/com/example/myfirstapplication/MyClass.java
+++ app/src/main/java/com/example/myfirstapplication/MyClass.java
@@ -3,6 +3,7 @@ package com.example.myfirstapplication;
 public class MyClass {
     static String str ="Dmitry";
     private String login;
+    private String password;
 
     public static void main(String[] args) {
         System.out.println("Hello"+str);

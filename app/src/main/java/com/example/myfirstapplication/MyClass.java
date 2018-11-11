6b4e700c9bd4b3ac19162e916fa030101b41fe5e
Dmitry Odinokov <ostbit@yandex.ru>
2018-11-11 23:54:02 +0300

    commit login

--- app/src/main/java/com/example/myfirstapplication/MyClass.java
+++ app/src/main/java/com/example/myfirstapplication/MyClass.java
@@ -2,6 +2,7 @@ package com.example.myfirstapplication;
 
 public class MyClass {
     static String str ="Dmitry";
+    private String login;
 
     public static void main(String[] args) {
         System.out.println("Hello"+str);
